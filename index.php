13c6335757f89f1e5d627d62b77aa1492294e209
chloe paolacci <chloe.pao74@gmail.com>
2019-03-07 17:05:08 +0100

    intialize

--- index.php
+++ index.php
@@ -7,14 +7,79 @@
     </head>
 
     <body>
-      <header>
-        <div class="head">
-        <a href="#"><img src="images/powerfreak_logo.png" alt="logo" /></a>
+      <header class="head">
 
+        <div class="langage">
+        <p> <a href="#" title="francais">FR</a> |  <a href="#" title="english">EN</a>  | <a href="#" title="deutch"> DE </a> |  <a href="#" title="italian">IT</a>  </p>
         </div>
+
+        <div class="shop">
+          <a href="#"><img src="images/shop-icon.png" alt="shop" width="35px" /></a>
+          <p>Shop</p>
+        </div>
+
+        <div class="logo">
+        <a href="#"><img src="images/powerfreak_logo.png" alt="logo" width="300px"/></a>
+        </div>
+
+        <nav class="menu">
+          <ul>
+            <li>
+              <a href="#" title="Home-Page">Home</a>
+            </li>
+            <li>
+              <a href="#" title="Priorite-recyclage">Priorité Recyclage</a>
+            </li>
+            <li>
+              <a href="#" title="PowerFreak">PowerFreak</a>
+            </li>
+            <li>
+              <a href="#" title="Contact">Contactez-Nous</a>
+            </li>
+          </ul>
+        </nav>
+
       </header>
       <!-- **HEADER** -->
 
+      <div class="section1">
+        <img src="images/visuel_section.png" alt="nouveauté" />
+        <h1> <em>100%</em> Recyclable  |  <em>100%</em> Eco-Responsable<br> <em>100%</em> Made In Valais</h1>
+      </div>
+      <!-- **SECTION 1** -->
+
+      <div class="section2">
+        <h2>Deux goûts <em>100%</em> naturels</h2>
+        <p>Venez découvrir nos parfums incontournables de saison</p>
+      </div>
+      <!-- **SECTION 2** -->
+
+      <div class="section3">
+        <p><em>Partout dans le monde</em>, la réputation des produits <em>Swiss made</em> est inégalée.<br> Savoir-faire, qualité irréprochable, esthétique parfaite,<br> innovation technique : l'indication Swiss made traduit tout cela, et bien plus encore.</p>
+        <div class="icon">
+          <h3>Swiss Made</h3>
+          <img src="images/icon-loc.png" alt="logo" width="50px"/>
+        </div>
+      </div>
+      <!-- **SECTION 3** -->
+
+      <div class="foot">
+        <p>Sign up to our Newsletter</p>
+        <ul>
+          <li>
+            <img src="images/icon-fb.png" alt="logo" width="30px"/>
+          </li>
+          <li>
+            <img src="images/icon-insta.png" alt="logo" width="30px"/>
+          </li>
+          <li>
+            <img src="images/icon-twitter.png" alt="logo" width="30px"/>
+          </li>
+        </ul>
+        <p>©PowerFreak</p>
+          <p class="link"> <a href="#" title="Sell">SELL OUR DRINK</a> |  <a href="#" title="Privacy-policy">PRIVACY POLICY</a>  | <a href="#" title="Faq"> FAQ </a> |  <a href="#" title="Mentions-legales">MENTIONS LEGALES</a>  </p>
+      </div>
+      <!-- **FOOTER** -->
     </body>
 
 </html>
