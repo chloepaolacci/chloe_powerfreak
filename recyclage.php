b3d70cea49cf7bc2a32355cbf6fcdbd0c10d47a2
chloe paolacci <chloe.pao74@gmail.com>
2019-03-08 17:07:04 +0100

    initialize

--- recyclage.php
+++ recyclage.php
@@ -26,10 +26,31 @@ empreinte carbone tout au long du cycle de vie d’une canette.</em><p>
       <section class="section cans">
         <h2 class="title-1">LES CANETTES POWERFREAK</h1>
         <h3 class="title-2">Légères et 100% Recyclables</h2>
-        <p>« Le recyclage d'une canette nécessite 95% d'énergie en moins que la production d'une nouvelle canette. »</p>
+        <img src="images/icon-ecolo.png" alt="logo" width="250vw"/>
+        <h4>« Le recyclage d'une canette nécessite <em>95%</em> d'énergie en moins que la production d'une nouvelle canette. »</h4>
+        <details>
+          <summary>En Savoir +</summary>
+          <p>Power Freak a délibérément choisi d'utiliser des canettes en aluminium.<br>
+          Aujourd’hui, leur poids a diminué significativement comparé aux années précédentes, <br>
+          ce qui permet de préserver de précieuses matières premières. Une fois collectées,<br>
+          elles sont entièrement recyclées, à l'infini, sans aucune perte de qualité.<br>
+          <span>LA CANETTE, UNE RÉSERVE D'ÉNERGIE</span>
+          Toutes les canettes sont recyclables à 100 % : une fois collectées et parvenues aux sites de recyclage, pas le moindre gramme d'aluminium ne se perd.  On peut la faire fondre et la réutiliser à l'infini sans aucune perte de qualité. Le recyclage de l'aluminium récupéré permet d'économiser 95 % d'énergie par rapport à l'utilisation d'aluminium issu des réserves naturelles.
+          Aujourd’hui, le poids d'une canette de Power Freak a diminué significativement comparé aux années précédentes. La diminution constante du poids fait aussi la différence : moins on utilise d'aluminium dans les canettes en début de cycle, plus petite sera l'empreinte carbone.
+          </p>
+          </details>
       </section>
       <!-- **SECTION 2** -->
 
+      <section class="section wall">
+        <h5 class="title-1">Production en Wall-to Wall</h5>
+        <h6 class="title-2">Economiser les ressources en passant par de courtes distances</h6>
+        <p>La production en « wall to wall » permet chaque année d'économiser plus de<br>
+          1643 tonnes d'émissions de CO2 par an <br>
+          Cela signifie que la production et le remplissage ont lieu sur le même site,<br>
+          ce qui fait beaucoup de kilomètres de transport économisés..</p>
+      </section>
+      <!-- **SECTION 3** -->
       <?php include 'partials/footer.php'; ?>
 
     </body>
